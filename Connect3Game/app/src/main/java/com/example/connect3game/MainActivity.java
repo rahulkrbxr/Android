01e56961b296b41ebc3eaf796673e311985f3916
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-07-15 03:15:03 +0530

    d

--- Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
+++ Connect3Game/app/src/main/java/com/example/connect3game/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.connect3game;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,37 +19,83 @@ public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
 
+    boolean gameActive = true;
+
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
-        Log.i("Tag", counter.getTag().toString());
-
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
-        gameState[tappedCounter] = activePlayer;
 
-        counter.setTranslationY(-1500);
+        if (gameState[tappedCounter] == 2 && gameActive) {
 
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
+            gameState[tappedCounter] = activePlayer;
 
-        } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
-        }
+            counter.setTranslationY(-1500);
 
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
 
-        for (int[] winningPosition : winningPositions) {
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
 
-            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
 
-                // Someone has won!
+            for (int[] winningPosition : winningPositions) {
 
-                Log.i("Tag", "Someone has won!");
-                Toast.makeText(this, "Someone has won!", Toast.LENGTH_SHORT).show();
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+
+                    // Someone has won!
+                    gameActive = false;
+
+                    String winner;
+
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+                    winnerTextView.setText(winner + " has won!");
+
+                    playAgainButton.setVisibility(View.VISIBLE);
+
+                    winnerTextView.setVisibility(View.VISIBLE);
+
+                }
             }
         }
+    }
+
+    public void playAgain(View view) {
+
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+        playAgainButton.setVisibility(View.INVISIBLE);
+
+        winnerTextView.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+        for (int i=0; i<gameState.length; i++) {
+            gameState[i] = 2;
+        }
+        activePlayer = 0;
+        gameActive = true;
     }
 
     @Override
