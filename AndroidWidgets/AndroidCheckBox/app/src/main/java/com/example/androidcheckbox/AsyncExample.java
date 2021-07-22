321c4f6c95b4c8e649d77c85f43c6ad2cfd06b3f
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-07-22 16:42:31 +0530

    dgrc

---
+++ AndroidWidgets/AndroidCheckBox/app/src/main/java/com/example/androidcheckbox/AsyncExample.java
@@ -0,0 +1,13 @@
+package com.example.androidcheckbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsyncExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_example);
+    }
+}
