bea0a51dbd4add25d30fe5fae946b3a75b2163c4
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-07-29 17:54:53 +0530

    dgrc

---
+++ Fragments/app/src/main/java/com/example/fragments/fragment1.java
@@ -0,0 +1,26 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class fragment1 extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+
+//        Button button = view.findViewById(R.id.fragment1btn);
+
+        return view;
+    }
+}
