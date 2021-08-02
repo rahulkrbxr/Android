897a699e066b083ea5188b308d3be1c837ba710b
Rahul Kumar <rahulkumarb4ubxr@gmail.com>
2021-08-02 17:02:47 +0530

    dgrc

---
+++ AndroidAlarmManager/app/src/main/java/com/example/androidalarmmanager/MyBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package com.example.androidalarmmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mediaPlayer = MediaPlayer.create(context, R.raw.service_bell);
+        mediaPlayer.start();
+        Toast.makeText(context, "Alarm...", Toast.LENGTH_LONG).show();
+    }
+}
